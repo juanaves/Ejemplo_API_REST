9b1edb38dcf0d0acc2d913ed9f3f15cc92a92da3
juanaves <juanaves@gmail.com>
2020-05-11 00:53:41 +0200

    ok

--- productos.py
+++
@@ -1,5 +0,0 @@
-productos = [
-    {'nombre': 'portatil', 'precio': 800, 'cantidad': 4},
-    {'nombre': 'raton', 'precio': 40, 'cantidad': 10},
-    {'nombre': 'monitor', 'precio': 400, 'cantidad': 3}
-]
