9074639edde8afc7f3f39f920d98dc6109d64c63
juanaves <juanaves@gmail.com>
2020-05-11 00:53:06 +0200

    ok

---
+++ productos.py
@@ -0,0 +1,5 @@
+productos = [
+    {'nombre': 'portatil', 'precio': 800, 'cantidad': 4},
+    {'nombre': 'raton', 'precio': 40, 'cantidad': 10},
+    {'nombre': 'monitor', 'precio': 400, 'cantidad': 3}
+]
